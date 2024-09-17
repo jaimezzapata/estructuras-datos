fcbea2470373176fe4fc860b8e9d77bdb563bca7
jaimezzzapata <medicenzapata@gmail.com>
2024-09-17 08:52:58 -0500

    Implementación de recorrido y llenado de arreglos

--- script.js
+++ script.js
@@ -8,22 +8,60 @@
 // console.log(numeros[3])
 // console.log(numeros[4])
 // console.log(numeros[5])
-function unaFuncion(){
-    console.log('Esto es una funcion')
+// function unaFuncion(){
+//     console.log('Esto es una funcion')
+// }
+// let numeros = [1,5,'caso',unaFuncion,2,8,9,true,20,4,5,false]
+// if(numeros[0] == 1){
+//     console.log('Numeros iguales')
+// }
+// numeros[3]()
+// switch (numeros[2]) {
+//     case 9:
+//         break;
+//     case 'caso':
+//         console.log('Esta es la posición 2')
+//         break; 
+//     default:
+//         break;
+// }
+// console.log(unaFuncion)
+// unaFuncion()
+
+// let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+// for (let index = 0; index < 9; index++) {
+//     if (numeros[index] % 2 == 0) {
+//         console.log('El número: ' + numeros[index] + ' es un número par')
+//     }
+// }
+
+// let estudiantes = ['Lina', 'Paola', 'Carol', 'Emmanuel', 'David', 'Mateo']
+// let estudiante = prompt('Ingrese el nombre del estudiante: ')
+// for (let index = 0; index < 6; index++) {
+//     if (estudiante == estudiantes[index]) {
+//         console.log('El estudiante ' + estudiante + ' existe en la base de datos')
+//     }
+// }
+
+// let personas = ['Camila', 'Luisa', 'Felipe', 'Ana', 'Tomas', 'Andrea']
+// let edades = [17, 22, 35, 15, 23, 18]
+// for (let index = 0; index < 6; index++) {
+//     if (edades[index] >= 18) {
+//         console.log('La persona ' + personas[index] + ' tiene ' + edades[index] + ' años')
+//     }
+// }
+
+let personas = []
+let edades = []
+for (let index = 0; index < 5; index++) {
+    let persona = prompt('Ingrese el nombre de la persona: ')
+    let edad = prompt('Ingrese la edad de la persona: ')
+    personas[index] = persona
+    edades[index] = edad
 }
-let numeros = [1,5,'caso',unaFuncion,2,8,9,10,20,4,5,6]
-if(numeros[0] == 1){
-    console.log('Numeros iguales')
+for (let index = 0; index < 5; index++) {
+    if (edades[index] >= 18) {
+        console.log('La persona ' + personas[index] + ' tiene ' + edades[index] + ' años')
+    }
 }
-numeros[3]()
-switch (numeros[2]) {
-    case 9:
-        break;
-    case 'caso':
-        console.log('Esta es la posición 2')
-        break; 
-    default:
-        break;
-}
-console.log(unaFuncion)
-unaFuncion()
